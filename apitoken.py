69220e9801aedca079c1364acc85f9585bb2f4b5
balamut152 <balamut1521@gmail.com>
2022-04-11 23:50:16 +0300

    My first test app blog

---
+++ apitoken.py
@@ -0,0 +1,16 @@
+import requests
+username = 'balamut152'
+token = '3213c4300428334a65942ecefd37aca4cab50608'
+
+response = requests.get(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/cpu/'.format(
+        username=username
+    ),
+    headers={'Authorization': 'Token {token}'.format(token=token)}
+)
+if response.status_code == 200:
+    print('CPU quota info:')
+    print(response.content)
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+ 
